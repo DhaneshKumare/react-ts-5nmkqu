a950f6bf3bad7717ea52e973e0e14af36e573195
DhaneshKumare <46071176+DhaneshKumare@users.noreply.github.com>
2023-04-03 14:39:29 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+import { TodoWrapper } from './Components/TodoWrapper';
+
+export default function App() {
+  return (
+    <div>
+      <TodoWrapper />
+    </div>
+  );
+}
